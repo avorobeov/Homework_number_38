98c96b8f89ac537614f00970e8b6f108b9c42f34
воробйов александр <avorobeov21@gmail.com>
2023-05-10 14:07:31 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_38/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 100;
+            int damage = 100;
+            int armour = 100;
+
+            Hero hero = new Hero(health, damage, armour);
+
+            hero.ShowHeroInfo();
+        }
+    }
+
+    class Hero
+    {
+        private int _health;
+        private int _damage;
+        private int _armour;
+
+        public Hero(int health, int damage, int armour)
+        {
+            _health = health;
+            _damage = damage;
+            _armour = armour;
+        }
+
+        public void ShowHeroInfo()
+        {
+            Console.WriteLine($"XP -{_health} DM -{_damage} AR - {_armour}");
+        }
+    }
+}
